a2a1e0ee43fc2ac0e3c6189f83c46fed3b805fab
Sohan <sohan.karfa@gmail.com>
2025-09-16 17:42:17 +0000

    - create a .env file - add the firebase config in the .env - use those v

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,10 +1,10 @@
+
 // It is safe to expose this configuration to the client
 export const firebaseConfig = {
-  "projectId": "studio-2336650890-b2dab",
-  "appId": "1:409225849807:web:a5f02685962db35cea40ba",
-  "storageBucket": "studio-2336650890-b2dab.firebasestorage.app",
-  "apiKey": "AIzaSyC3pDHUA1V3S56-bcFJkTQTCaiTqCg2vLM",
-  "authDomain": "studio-2336650890-b2dab.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "409225849807"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
